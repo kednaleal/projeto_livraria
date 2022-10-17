886a4c84f4191c12a225255827e67b67fde1ee2f
gabrielsszz <gabrielfndsts265@gmail.com>
2022-10-17 19:47:47 -0300

    começando...

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const btnPesquisa = document.getElementById('pesquisa')
+const  txtprocura =  document.getElementById('procura')
+
+btnPesquisa.addEventListener('click', () => {
+  console.log('clicou')
+})
+txtprocura.addEventListener('click', () =>{
+  console.log('texto')
+ 
+})
+
