a71662b211cdee180c639566421473be2fb4f110
gabrielsszz <gabrielfndsts265@gmail.com>
2022-10-21 21:16:27 -0300

    quase...terminado.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 const btnPesquisa = document.getElementById('pesquisa')
 const  txtprocura =  document.getElementById('procura')
+const  livros =  document.getElementById('livros')
 
-btnPesquisa.addEventListener('click', () => {
-  console.log('clicou')
-})
-txtprocura.addEventListener('click', () =>{
-  console.log('texto')
+
+btnPesquisa.addEventListener('click', async () => {
+ const pesquisa = txtprocura.value.replaceAll(' ', '+')
+ const res = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${pesquisa}`)
+ const dados = await res.json()
  
+ console.log(dados.items)
+ 
+ livros.innerHTML = '';
+ dados.items.forEach(item => {
+  livros.innerHTML = livros.innerHTML + `<li>${item.volumeInfo.title} -  ${item.volumeInfo.authors}</li>`
+ });
 })
+
 
